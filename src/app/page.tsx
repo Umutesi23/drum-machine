96f51994ad2719890bd36c5106070efd63fe43dd
Novice <novice@Xcode-Air.local>
2024-04-16 10:15:49 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,193 @@
+"use client";
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import logo from "./images/logo.png";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import { Slider } from "@/components/ui/slider";
+import { useState } from "react";
+import useSound from "use-sound";
+//IMPORTS
 export default function Home() {
+  const volumeSlider = document.getElementById("volume-slider");
+
+  const Heater1 = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  );
+  const Heater2 = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  );
+  const Heater3 = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  );
+  const Heater4 = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  );
+  const Clap = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  );
+  const OpenHH = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  );
+  const PunchyKick = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  );
+  const SideStick = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  );
+  const Snare = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  );
+  //power on off
+  const [cPower, setPower] = useState(true);
+  function powerHandler() {
+    cPower ? setPower(false) : setPower(true);
+    setSound("");
+  }
+  //the name of sound
+  const [currentSound, setSound] = useState("");
+  function soundHandler(e: any) {
+    cPower ? setSound(e.target.value) : setSound("");
+    e.target.value == "Heater1"
+      ? Heater1.play()
+      : e.target.value == "Heater2"
+      ? Heater2.play()
+      : e.target.value == "Heater3"
+      ? Heater3.play()
+      : e.target.value == "Heater4"
+      ? Heater4.play()
+      : e.target.value == "Clap"
+      ? Clap.play()
+      : e.target.value == "OpenHH"
+      ? OpenHH.play()
+      : e.target.value == "PunchyKick"
+      ? PunchyKick.play()
+      : e.target.value == "SideStick"
+      ? SideStick.play()
+      : e.target.value == "Snare"
+      ? Snare.play()
+      : "";
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main
+      className="flex min-h-screen flex-col items-center justify-between p-24 bg-[gray]"
+      id="drum-machine"
+    >
+      <div
+        className=" border-[0.7vh] m-auto h-[40vh] w-[45vw] border-[orange] bg-neutral-400 flex"
+        id="display"
+      >
+        <div className="  w-[60%] h-[100%] flex flex-wrap	p-[2vw] gap-[0.6vw]">
+          <Button
+            className=" font-semibold text-[1.5vw] bg-[gray] w-[30%] h-[30%] drum-pad"
+            id="Q"
+            value="Heater1"
+            onClick={soundHandler}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Q
+          </Button>
+
+          <Button
+            className=" font-semibold text-[1.5vw] bg-[gray] w-[30%] h-[30%] drum-pad"
+            id="W"
+            value="Heater2"
+            onClick={soundHandler}
+          >
+            W
+          </Button>
+          <Button
+            className=" font-semibold text-[1.5vw] bg-[gray] w-[30%] h-[30%] drum-pad"
+            id="E"
+            value="Heater3"
+            onClick={soundHandler}
+          >
+            E
+          </Button>
+          <Button
+            className=" font-semibold text-[1.5vw] bg-[gray] w-[30%] h-[30%] drum-pad"
+            id="A"
+            value="Heater4"
+            onClick={soundHandler}
+          >
+            A
+          </Button>
+          <Button
+            className=" font-semibold text-[1.5vw] bg-[gray] w-[30%] h-[30%] drum-pad"
+            id="S"
+            value="Clap"
+            onClick={soundHandler}
+          >
+            S
+          </Button>
+          <Button
+            className=" font-semibold text-[1.5vw] bg-[gray] w-[30%] h-[30%] drum-pad"
+            id="D"
+            value="OpenHH"
+            onClick={soundHandler}
+          >
+            D
+          </Button>
+          <Button
+            className=" font-semibold text-[1.5vw] bg-[gray] w-[30%] h-[30%] drum-pad"
+            id="Z"
+            value="PunchyKick"
+            onClick={soundHandler}
+          >
+            Z
+          </Button>
+          <Button
+            className=" font-semibold text-[1.5vw] bg-[gray] w-[30%] h-[30%] drum-pad"
+            id="X"
+            value="SideStick"
+            onClick={soundHandler}
+          >
+            X
+          </Button>
+          <Button
+            className=" font-semibold text-[1.5vw] bg-[gray] w-[30%] h-[30%] drum-pad"
+            id="C"
+            value="Snare"
+            onClick={soundHandler}
+          >
+            C
+          </Button>
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className=" block p-[0.8vw] w-[40%]">
+          <div className=" w-[100%] flex justify-end	">
+            <Image src={logo} alt="logo" className=" w-[2vw] h-[2vw]"></Image>
+          </div>
+          <div className=" justify-center w-[100%] flex flex-wrap gap-x-[10vw] gap-y-[1vw]">
+            <Label className=" font-extrabold text-[1vw]">Power</Label>
+            <Switch
+              id="airplane-mode"
+              className="data-[state=checked]:bg-[gray] data-[state=unchecked]:bg-black"
+              defaultChecked
+            />
+          </div>
+          <br></br>
+          <div className=" h-[6vh] w-[70%] bg-[grey] m-auto font-bold text-[1.3vw] text-center p-[0.7vw]">
+            {currentSound}
+          </div>
+          <br></br>
+          <Slider
+            defaultValue={[33]}
+            max={100}
+            step={1}
+            className="bg-[gray] w-[100%]"
+            typeof="range"
+            id="volume-slider"
+          />
+          <br></br>
+          <div className=" justify-center w-[100%] flex flex-wrap gap-x-[12vw] gap-y-[1vw]">
+            <Label className=" font-extrabold text-[1vw]">Bank</Label>
+            <Switch
+              id="airplane-mode"
+              className="data-[state=checked]:bg-[gray] data-[state=unchecked]:bg-black"
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
